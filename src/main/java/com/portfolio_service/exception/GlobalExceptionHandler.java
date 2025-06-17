0d08fab1460d6a29d9944c6a957098a3141fad6a
Tanish <yesebuadp@gmail.com>
2025-06-17 10:58:55 +0530

    Added handleGeneralException

--- src/main/java/com/portfolio_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/portfolio_service/exception/GlobalExceptionHandler.java
@@ -13,6 +13,9 @@ public class GlobalExceptionHandler extends RuntimeException {
     public ResponseEntity<String> handleNotFound(Exception ex){
         return new ResponseEntity(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
-
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return new ResponseEntity<>("Something went wrong: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
