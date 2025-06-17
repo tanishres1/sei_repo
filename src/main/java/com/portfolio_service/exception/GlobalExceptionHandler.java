8cd23387817f78fe2652f42b43f6fdd9a56540d8
Tanish <yesebuadp@gmail.com>
2025-06-17 10:36:11 +0530

    Added GlobalExceptionHandler.java to dev branch

--- src/main/java/com/portfolio_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/portfolio_service/exception/GlobalExceptionHandler.java
@@ -1,6 +1,18 @@
 package com.portfolio_service.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler extends RuntimeException {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleNotFound(Exception ex){
+        return new ResponseEntity(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
 
 
 }
